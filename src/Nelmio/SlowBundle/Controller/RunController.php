716428ae1e5b561730492838e069e6c64ce3a09c
Magnus Nordlander <magnus@nordlander.se>
2011-10-20 17:22:34 +0200

    a

--- src/Nelmio/SlowBundle/Controller/RunController.php
+++ src/Nelmio/SlowBundle/Controller/RunController.php
@@ -5,6 +5,7 @@ namespace Nelmio\SlowBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 
 class RunController extends Controller
 {
